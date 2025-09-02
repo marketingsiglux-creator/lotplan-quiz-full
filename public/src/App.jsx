fdb6986fb77c3bd7a508f175ba2cd32244aa59ae
marketingsiglux-creator <marketing.siglux@gmail.com>
2025-09-02 14:19:03 -0400

    Add src/App.jsx

---
+++ public/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LotPlanServiceQuiz from './components/LotPlanServiceQuiz';
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-4">
+      <LotPlanServiceQuiz />
+    </main>
+  );
+}
+
+export default App;
